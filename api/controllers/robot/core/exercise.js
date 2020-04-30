f2b9faabecf74e8b460dee433c10ae4f70d714cd
karat.home <xwlxyjk@gmail.com>
2020-04-30 23:32:30 +0800

    Clean up

--- api/controllers/robot/core/exercise.js
+++ api/controllers/robot/core/exercise.js
@@ -12,21 +12,11 @@ function doEx(){
     // age
     eve.learn('what is your age::0.1');
     eve.learn('tell me your age::0.1');
-    // eve.learn('your age::0.1');
     eve.learn('how old are you::I am 0.1');
-    //dirty
-    eve.learn('fuck you::fuck your self!');
-    eve.learn('fuck::come on');
-    eve.learn('fuck u::fuck your self!');
-    eve.learn('kill you::kill your mother!');
-    eve.learn('you sb::you too :（');
-    eve.learn('you are shit::i shit you');
-    eve.learn('you are sb::you too :（');
-
-    eve.learn('i want kill sb::kill ni mei a !sb');
-    eve.learn('i want kiss you::love you');
-    eve.learn('do you want eat shit::yes, i love shit');
-    eve.learn('do you want eat apple::apple your sister');
+    eve.learn('Good morning!::Morning!');
+    eve.learn('Good afternoon!::Good afternoon!');
+    eve.learn('Good night!::Good night!');
+    eve.learn('Have a good day!::Thanks! Good day!');
 }
 
 exports.init = function(eveMod){
